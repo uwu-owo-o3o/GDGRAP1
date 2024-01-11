53f4db5344949f5d6c1bb388e03e011c74bc0469
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-01-11 22:11:55 +0800

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,44 @@
+#include "GLFW/glfw3.h"
+
+int main(){
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    // initialization occurs in this section //
+
+    /* Loop until the user closes the window */
+    // the while loop below is also the render loop //
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents(); // processes user inputs //
+    }
+
+    glfwTerminate();
+    return 0;
+}
