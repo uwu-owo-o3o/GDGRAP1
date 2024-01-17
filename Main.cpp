51e2ce7864a8e32ad7b04de5b51a30a7d61e43ff
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-01-17 10:49:30 +0800

    VAOs, VBOs, and EBOs

--- Main.cpp
+++ Main.cpp
@@ -24,10 +24,16 @@ int main(){
         -0.5f, -0.5f, 0.f,
         0.5f,   -0.5f, 0.f
     };
+
+    GLuint indices[]{
+        0, 1, 2
+    };
     
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
+
     glGenVertexArrays(1, &VAO); // line responsible for VAO
     glGenBuffers(1, &VBO); // line responsible for VBO
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO); // assigns VAO currently being edited
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // assigns VBO currently being edited and attaches VBO to VAO
@@ -35,17 +41,24 @@ int main(){
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+
 
         glEnd();
 
@@ -56,6 +69,7 @@ int main(){
     
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
