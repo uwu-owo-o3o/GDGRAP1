820cbdff328e0229b885c2e16bd56b6386d3cb3d
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-01-17 10:26:58 +0800

    "3d" triangle

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,6 @@
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include "Calculator.hpp"
 
-using namespace calculator;
 
 int main(){
     GLFWwindow* window;
@@ -17,20 +16,36 @@ int main(){
     }
 
     glfwMakeContextCurrent(window);
+    gladLoadGL();
 
-    Calculator calculator = Calculator();
+    GLfloat vertices[]{
+      // x,    y,    z 
+        0.f, 0.5f, 0.f,
+        -0.5f, -0.5f, 0.f,
+        0.5f,   -0.5f, 0.f
+    };
+    
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO); // line responsible for VAO
+    glGenBuffers(1, &VBO); // line responsible for VBO
+
+    glBindVertexArray(VAO); // assigns VAO currently being edited
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // assigns VBO currently being edited and attaches VBO to VAO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // sizeof(vertices) -> size of array in bytes, GL_STATIC_DRAW as the model doesn't move
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_POLYGON);
-        
-        glVertex2f((*calculator.getXPointsArray())[0], (*calculator.getYPointsArray())[0]);
-        glVertex2f((*calculator.getXPointsArray())[1], (*calculator.getYPointsArray())[1]);
-        glVertex2f((*calculator.getXPointsArray())[2], (*calculator.getYPointsArray())[2]);
-        glVertex2f((*calculator.getXPointsArray())[3], (*calculator.getYPointsArray())[3]);
-        glVertex2f((*calculator.getXPointsArray())[4], (*calculator.getYPointsArray())[4]);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glEnd();
 
@@ -38,6 +53,9 @@ int main(){
 
         glfwPollEvents();
     }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
