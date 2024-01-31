6e9860e9e6e6122e2b6b436b3411cd570a9a0c3c
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-01-31 20:51:50 +0800

    bunny moves as intended

--- Main.cpp
+++ Main.cpp
@@ -7,26 +7,13 @@
 
 #include "iostream"
 #include "string"
+#include "windows.h"
+
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
 
 // when submitting .exe on release, put 3D folder in the Release folder //
-float x_mod = 0;
-
-
-void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-
-    if (key == GLFW_KEY_D && action == GLFW_PRESS) {
-        std::cout << "You pressed D" << std::endl;
-        x_mod += 0.1f;
-    }
-
-    if (key == GLFW_KEY_A && action == GLFW_PRESS) {
-        std::cout << "You pressed A" << std::endl;
-        x_mod -= 0.1f;
-    }
-}
 
 int main() {
     GLFWwindow* window;
@@ -43,7 +30,6 @@ int main() {
 
     glfwMakeContextCurrent(window);
     gladLoadGL();
-    glfwSetKeyCallback(window, Key_Callback);
 
     std::fstream vertSrc("Shaders/sample.vert");
     std::stringstream vertBuff;
@@ -125,32 +111,146 @@ int main() {
 
     glm::mat4 identity_matrix = glm::mat4(1.0f);
 
-    float x = 0;
-    float y = 0;
-    float z = 0;
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
 
-    float scale_x = 5;
-    float scale_y = 5;
-    float scale_z = 1;
+    float scale_x = 0.5f;
+    float scale_y = 0.5f;
+    float scale_z = 1.f;
 
-    float theta = 90;
-    float axis_x = 0;
-    float axis_y = 1;
-    float axis_z = 0;
+    float theta = 90.f;
+    float axis_x = 0.f;
+    float axis_y = 1.f;
+    float axis_z = 0.f;
+
+    float x_check1 = 0.0f;
+    float y_check1 = 0.5f;
+  
+    float x_check2 = 0.5f;
+    float y_check2 = 0.0f;
+
+    float x_check3 = 0.0f;
+    float y_check3 = -0.5f;
+
+    float x_check4 = -0.5f;
+    float y_check4 =  0.0f;
+
+    bool move = false;
+    bool hasPassedCheck1 = false;
+    bool hasPassedCheck2 = false;
+    bool hasPassedCheck3 = false;
+    bool hasPassedCheck4 = false;
+
+    float x_mod = 0.0f;
+    float y_mod = 0.5f;
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+      
+
+        if (move == false) {
+            x = 0.0f;
+            y = y_mod;
+            move = true;
+        }
+
+        // updates flags
+        if (x == x_check1 && y == y_check1) {
+            std::cout << "passed check point 1" << std::endl;
+            hasPassedCheck1 = true;
+            hasPassedCheck2 = false;
+            hasPassedCheck3 = false;
+            hasPassedCheck4 = false;
+        }
+
+        if (x == x_check2 && y == y_check2) {
+            std::cout << "passed check point 2" << std::endl;
+            hasPassedCheck1 = false;
+            hasPassedCheck2 = true;
+            hasPassedCheck3 = false;
+            hasPassedCheck4 = false;
+        }
+
+        if (x == x_check3 && y == y_check3) {
+            std::cout << "passed check point 3" << std::endl;
+            hasPassedCheck1 = false;
+            hasPassedCheck2 = false;
+            hasPassedCheck3 = true;
+            hasPassedCheck4 = false;
+        }
+        
+        if (x == x_check4 && y == y_check4) {
+            std::cout << "passed check point 4" << std::endl;
+            hasPassedCheck1 = false;
+            hasPassedCheck2 = false;
+            hasPassedCheck3 = false;
+            hasPassedCheck4 = true;
+        }
+
+        Sleep(300);
+
+        // updates coordinates for movement
+        if (hasPassedCheck1 == true) {
+            std::cout << "successfully passed check 1 "<< std::endl;
+            x_mod += 0.1f;
+            if (x_mod == 0.5f) {
+                std::cout << "entered correction" << std::endl;
+                y_mod = 0.0f;
+            }
+            else {
+                y_mod -= 0.1f;
+            }  
+        }
+
+        else if (hasPassedCheck2 == true) {
+            std::cout << "successfully passed check 2 " << std::endl;
+            y_mod -= 0.1f;
+            if (y_mod == -0.5f) {
+                std::cout << "entered correction" << std::endl;
+                x_mod = 0.0f;
+            }
+            else {
+                x_mod -= 0.1f;
+            }
+        }
+
+        else if (hasPassedCheck3 == true) {
+            std::cout << "successfully passed check 3 " << std::endl;
+            x_mod -= 0.1f;
+            if (x_mod == -0.5f) {
+                std::cout << "entered correction" << std::endl;
+                y_mod = 0.0f;
+            }
+            else {
+                y_mod += 0.1f;
+            }
+        }
+       
+        else if (hasPassedCheck4 == true) {
+            std::cout << "successfully passed check 4 " << std::endl;
+            y_mod += 0.1f;
+            if (y_mod == 0.5f) {
+                std::cout << "entered correction" << std::endl;
+                x_mod = 0.0f;
+            }
+            else {
+                x_mod += 0.1f;
+            }
+        }
+
+        x = x_mod;
+        std::cout << "check x value: " << x << std::endl;
+        y = y_mod;
+        std::cout << "check y value: " << y << std::endl;
 
         // start with translation matrix //
         glm::mat4 transformation_matrix = glm::translate(identity_matrix, glm::vec3(x, y, z));
 
         // multiply matrix with scale matrix //
         transformation_matrix = glm::scale(transformation_matrix, glm::vec3(scale_x, scale_y, scale_z));
-
-        // multiply with rotate matrix //
-        transformation_matrix = glm::rotate(transformation_matrix, glm::radians(theta), glm::normalize(glm::vec3(axis_x, axis_y, axis_z)));
-        
 
         unsigned int transformLoc = glGetUniformLocation(shaderProg, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transformation_matrix));
@@ -164,7 +264,7 @@ int main() {
 
         glfwSwapBuffers(window);
 
-        glfwPollEvents();
+        glfwPollEvents();    
     }
 
     glDeleteVertexArrays(1, &VAO);
@@ -174,3 +274,5 @@ int main() {
     glfwTerminate();
     return 0;
 }
+
+
